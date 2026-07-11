ae065c3e661bb862ee8de6cc7939441d5411df3d
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2026-07-11 21:11:29 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+
+        let i = 0;
+        let j = nums.length - 1
+
+        while(i <= j) {
+            let mid = Math.floor((i+j)/2)
+            if(nums[mid] == target) {
+                 return mid
+            }
+            if(nums[mid] < target){
+                i = mid + 1
+            }
+             if(nums[mid] > target){
+                j = mid - 1
+            }
+        }
+
+        return -1
+    }
+}
