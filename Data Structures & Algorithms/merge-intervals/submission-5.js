ed1d0b6d10319a4df3960994d50f668a0953f98c
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2026-07-23 23:43:13 +0530

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        if (intervals.length <= 1) return intervals
+        intervals.sort((a, b) => a[0] - b[0]);
+        const ans = []
+
+        let current = intervals[0];
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                ans.push(current);
+                current = intervals[i];
+            }
+        }
+
+        ans.push(current);
+
+        return ans
+    }
+}
